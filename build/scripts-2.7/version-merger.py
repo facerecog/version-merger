b5f1d4f99b1c913e510785ddaa166f1430d2fc99
Your Name <you@example.com>
2016-03-08 15:34:06 +0800

    Version 1.0.3 uploaded to pip

--- build/scripts-2.7/version-merger.py
+++ build/scripts-2.7/version-merger.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!python
 
 import subprocess
 import time
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.0',
+    version='1.0.3',
 
     description='Automatically track and save regularly changing files.',
     long_description=long_description,
@@ -88,6 +88,7 @@ setup(
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
     data_files=[
+            # ('/usr/bin', ['version-merger.py'])
             # ('my_data', ['data/data_file'])
             ],
 
@@ -99,4 +100,6 @@ setup(
             # # 'sample=sample:main',
         # # ],
     # },
+
+    scripts=['version-merger.py']
 )
